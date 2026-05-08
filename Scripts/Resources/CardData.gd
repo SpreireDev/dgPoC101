5beadf13d7a364b078fe88b650227e0ae5b979ec
SpreireDev <spreire@protonmail.com>
2026-05-07 19:41:47 -0500

    Stage 1: Updated CardData.gd + new CardEffect.gd + created 9 data-driven card .tres files from table

--- Scripts/Resources/CardData.gd
+++ Scripts/Resources/CardData.gd
@@ -2,14 +2,14 @@ class_name CardData
 extends Resource
 
 @export var card_name: String = "Strike"
-@export var card_type: String = "Attack"  # "Attack", "Defend", "Power Atk", "Buff"
-@export var tickdown: int = 3
+@export var card_type: String = "Attack"          # "Attack", "Defend", "Power Atk", "Buff"
+@export var tickdown: int = 3                     # used as enemy telegraph time (player draw timer unchanged)
 @export var damage: int = 5
 @export var guard: int = 0
-@export var mana_add: int = 0
+@export var mana_add: int = 0                     # instant mana gain on play
 @export var mana_cost: int = 1
 @export var requires_rage: bool = false
-@export var effects: Array[Resource] = []  # future CardEffect resources
+@export var effects: Array[Resource] = []         # future CardEffect resources
 
 func _to_string() -> String:
 	return "%s (%s)" % [card_name, card_type]
--- Scripts/Resources/CardEffect.gd
+++ Scripts/Resources/CardEffect.gd
@@ -4,5 +4,5 @@ extends Resource
 @export var effect_name: String = ""
 
 # Called when card resolves (override in subclasses later)
-func execute(_battle_manager: Node, _is_player: bool = true) -> void:
+func execute(_battle_manager: Node, _is_player: bool) -> void:
 	pass
